c971f74a0230a624dc2cf334a05d413dee4c389e
Kenta Sato <bicycle1885@gmail.com>
2017-10-17 15:26:06 +0900

    direct export

--- src/FMIndexes.jl
+++ src/FMIndexes.jl
@@ -9,14 +9,6 @@ export
     locate,
     locateall
 
-import Base:
-    count,
-    length,
-    show,
-    start,
-    done,
-    next
-
 using SuffixArrays
 using WaveletMatrices
 using IndexableBitVectors
@@ -87,9 +79,9 @@ function FMIndex(text::String; opts...)
     return FMIndex(convert(Vector{UInt8}, text), 128; opts...)
 end
 
-length(index::FMIndex) = length(index.bwt)
+Base.length(index::FMIndex) = length(index.bwt)
 
-function show{w,T}(io::IO, fmindex::FMIndex{w,T})
+function Base.show{w,T}(io::IO, fmindex::FMIndex{w,T})
     print(io, "FMIndex{", w, ",", T, "}:\n")
     totalsize = (
         sizeof(fmindex.bwt) +
@@ -119,7 +111,7 @@ end
 """
 Count the number of occurrences of the given query.
 """
-function count(query, index::FMIndex)
+function Base.count(query, index::FMIndex)
     return length(sa_range(query, index))
 end
 
