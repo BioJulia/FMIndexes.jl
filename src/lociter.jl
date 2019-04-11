c14fa4a9d749548f29305a5bf0cd79d0da24fa4d
timbitz <tim.sterne.weiler@utoronto.ca>
2019-04-11 14:04:01 -0700

    init re-write of iterators for 0.7

--- src/lociter.jl
+++ src/lociter.jl
@@ -4,6 +4,10 @@ struct LocationIterator{w,T}
 end
 
 Base.length(iter::LocationIterator) = length(iter.range)
-@inline Base.start(iter::LocationIterator) = 1
-@inline Base.done(iter::LocationIterator, i) = i > length(iter)
-@inline Base.next(iter::LocationIterator, i) = sa_value(iter.range[i], iter.index) + 1, i + 1
+
+function Base.iterate(iter::LocationIterator, i::Int=1)
+    if i > length(iter)
+        return nothing
+    end
+    return sa_value(iter.range[i], iter.index) + 1, i + 1
+end
